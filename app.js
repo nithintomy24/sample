60c570a28a78a19b743c6e24a63e58c86a6fc2c3
nithintomy24 <nithintomy24@gmail.com>
2017-07-03 12:36:01 +0530

    added test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let app = module.exports = express();
 // Create a client towards InfluxDB
 let influx = new Influx.InfluxDB({
    host: process.env.INFLUXDB_HOST || "db",
-   database: 'sample'
+   database: process.env.INFLUXDB_DATABASE || 'iot'
 });
 
 // Body parser configuration
